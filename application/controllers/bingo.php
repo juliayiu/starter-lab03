a1fe59f75d607d9ddf399af60c64f3c1f1fe295e
Julia <jyiu93@gmail.com>
2015-09-26 19:43:02 -0700

    bingo link completed -- new controller bingo with regex 4letters/bingo

---
+++ application/controllers/bingo.php
@@ -0,0 +1,18 @@
+<?php
+
+class bingo extends Application
+{
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->data['pagebody'] = 'justone';  // this is the view we want shown
+        
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(5);
+        $this->data = array_merge($this->data, $source);
+        
+        $this->render();
+    }
+}
