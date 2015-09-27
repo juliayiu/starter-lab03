d221217aa4beb685fa35faf09757e4d8b2aeffb4
Julia <jyiu93@gmail.com>
2015-09-26 19:35:08 -0700

    dunno link completed -- remapped to guess (new controller)

---
+++ application/controllers/guess.php
@@ -0,0 +1,18 @@
+<?php
+
+class guess extends Application
+{
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->data['pagebody'] = 'justone';  // this is the view we want shown
+        
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(4);
+        $this->data = array_merge($this->data, $source);
+        
+        $this->render();
+    }
+}
